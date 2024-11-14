908bed7fb2e5016cdf82d63d8d0a0418cf8eb3e2
KieuAnh <vukieuanh.hnue@gmail.com>
2024-11-14 11:09:24 +0700

    pass test 30/4/2018

--- src/NextDayCalculator.java
+++ src/NextDayCalculator.java
@@ -4,8 +4,7 @@ public class NextDayCalculator {
     public static final int FIRSTOFMONTH = 1;
 
     public String getNextDay(int day, int month, int year) {
-
-        if (day==31){
+        if (day==31 || day==30){
             return FIRSTOFMONTH + CONCAT + (++month) + CONCAT + year;
         }
         return ++day + CONCAT + month + CONCAT + year;
